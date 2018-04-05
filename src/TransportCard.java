5e5cd6e577841e68494e0ea4d285579fbc4819bb
hakjoon <cho224621@gmail.com>
2018-04-05 22:46:50 +1000

    modify

--- src/TransportCard.java
+++ src/TransportCard.java
@@ -6,12 +6,16 @@ public class TransportCard {
 	private int money;
 	private int rideLocation;
 	private boolean inAndOut;
+	private int time;
+	private int busOrMetro; //0: 최초 1: 버스  2: 지하철
 
 	public TransportCard(String name){
 		this.name = name;
 		this.money = 0;
 		this.inAndOut = false;
 		this.rideLocation = 0;
+		this.busOrMetro = 0;
+		this.time = 0;
 	}
 	public void setMoney(int money){
 		this.money = money;
