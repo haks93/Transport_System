590811abf37d5682b71688eb4a3db1c5e2ac8747
hakjoon <cho224621@gmail.com>
2018-04-06 22:33:12 +1000

    modify checkMoney

--- src/Bus.java
+++ src/Bus.java
@@ -22,27 +22,27 @@ public class Bus extends PublicTransport {
         if(card.isInAndOut()){
             switch (card.getBusOrMetro()){
                 case 1:
-                    checkMoney(card, 1750);
+                    checkMoney(card, 1750, 1750);
                     break;
                 case 2:
-                    checkMoney(card, 1650);
+                    checkMoney(card, 1650, 1650);
                     break;
             }
         }else{
             //TODO: if(현재 시간 - 카드.시간 <= 15 && 지하철) 환승, 나머지 일반 탑승
             if(System.currentTimeMillis() - card.getTime() <= 15000 && card.getBusOrMetro() == 2){
-                checkMoney(card, 700);
+                checkMoney(card, 700, 0);
             }else{
-                checkMoney(card, 1750);
+                checkMoney(card, 1750, 1050);
             }
         }
     }
 
-    public void checkMoney(TransportCard card, int money) {
-        if (card.getMoney() < money) {
+    public void checkMoney(TransportCard card, int limit, int pay) {
+        if (card.getMoney() < limit) {
             System.out.println("잔액이 부족합니다.");
         }else{
-            card.setMoney(card.getMoney() - money);
+            card.setMoney(card.getMoney() - pay);
             card.setTime(System.currentTimeMillis());
             card.setBusOrMetro(1);
             card.setInAndOut(true);
