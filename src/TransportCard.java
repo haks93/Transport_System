3eb27713b06216261482d54d2f62f65fde7b2c85
hakjoon <cho224621@gmail.com>
2018-04-06 21:14:35 +1000

    modify variable time to tagTime

--- src/TransportCard.java
+++ src/TransportCard.java
@@ -6,7 +6,7 @@ public class TransportCard {
 	private int money;
 	private int rideLocation;
 	private boolean inAndOut;
-	private long time;
+	private long tagTime;
 	private int busOrMetro; //0: 최초 1: 버스  2: 지하철
 
 	public TransportCard(String name){
@@ -15,7 +15,7 @@ public class TransportCard {
 		this.inAndOut = false;
 		this.rideLocation = 0;
 		this.busOrMetro = 0;
-		this.time = 0;
+		this.tagTime = 0;
 	}
 	public void setMoney(int money){
 		this.money = money;
@@ -31,11 +31,11 @@ public class TransportCard {
 
 
 	public long getTime() {
-		return time;
+		return tagTime;
 	}
 
 	public void setTime(int time) {
-		this.time = time;
+		this.tagTime = time;
 	}
 
 	public int getBusOrMetro() {
