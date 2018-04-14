281287a3e48322fa0474b32be8bffd4ce31013c9
hakjoon <cho224621@gmail.com>
2018-04-14 17:10:40 +1000

    Add Circumstance class

--- src/Circumstance.java
+++ src/Circumstance.java
@@ -1,19 +1,37 @@
-public class Circumstance extends Thread{
+import java.util.*;
 
-    private long timeStamp;
+public class Circumstance extends TimerTask{
 
-    public Circumstance(long time){
-        this.timeStamp = time;
+    private ArrayList<TransportCard> cardList;
+    private ArrayList<PublicTransport> publicTransports;
+
+    /*
+    버스 객체와 지하철 객체 5개를 생성한 후 PublicTransport 배열에 넣는다.
+    교통카드 배열을 생성
+     */
+    public Circumstance(){
+        this.cardList = new ArrayList<TransportCard>();
+        this.publicTransports = new ArrayList<PublicTransport>();
+
+        Bus bus = new Bus("b01");
+        this.publicTransports.add(bus);
+
+        int id = 1;
+        for(int i = 0 ; i < 5; i++){
+            Metro metro = new Metro(Integer.toString(id + i));
+            this.publicTransports.add(metro);
+        }
+    }
+    @Override
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        String select;
+
+
     }
 
-    @Override
-    public void run() {
-        while (true){
-            while(timeStamp - System.currentTimeMillis() < 180000){
-
-
-            }
-            this.timeStamp = System.currentTimeMillis();
-        }
+    public TransportCard makdCard(String id){
+        TransportCard newCard = new TransportCard(id);
+        return newCard;
     }
 }
